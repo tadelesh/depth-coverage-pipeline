4e6674126928b124b64d78c595dce91e2b8ce64b
FAREAST\chunyu <chunyu@microsoft.com>
2021-01-28 18:33:34 +0800

    send email via sendgrid

---
+++ script/sendGridemail.py
@@ -0,0 +1,35 @@
+import argparse
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import codecs
+
+parser = argparse.ArgumentParser()
+parser.add_argument("content", help="email content")
+parser.add_argument("email", help="mail list")
+parser.add_argument("subject", help="mail subject")
+# parser.add_argument("release_version", help="release version")
+parser.add_argument("api_key", help="sendgrid api key")
+args = parser.parse_args()
+
+from_email = 'codegen@azure-devex-tools.com'
+to_emails = args.email.split('_')
+subject = args.subject
+html_content = args.content
+# subject = 'Onboading ' + args.release_version + ' is Released'
+# f = codecs.open(args.template, 'r')
+# html_content = f.read()
+# html_content = html_content.replace("trenton_version", args.release_version)
+
+message = Mail(
+    from_email=from_email,
+    to_emails=to_emails,
+    subject=subject,
+    html_content=html_content)
+try:
+    sg = SendGridAPIClient(api_key=args.api_key)
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
