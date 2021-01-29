c11df94b5eae6dc1b5e7226d7e1f06b676f2448d
FAREAST\chunyu <chunyu@microsoft.com>
2021-01-29 10:45:30 +0800

    onboard email

--- script/onboard.py
+++ script/onboard.py
@@ -1,16 +1,16 @@
 from jinja2 import Environment, FileSystemLoader
 import sys
 
-file_loader = FileSystemLoader('./')
-env = Environment(loader=file_loader)
+# file_loader = FileSystemLoader('./')
+# env = Environment(loader=file_loader)
 
-template = env.get_template('script/onboardTemplate')
-users = [{
-    "url": "url",
-    "username": "test"
-}]
-output = template.render(users)
-print(output)
+# template = env.get_template('script/onboardTemplate')
+# users = [{
+#     "url": "url",
+#     "username": "test"
+# }]
+# output = template.render(users)
+# print(output)
 
 def Generate_HTML(title, codePR, triggerPR, submitURL, customizeURL):
   file_loader = FileSystemLoader('./')
@@ -18,15 +18,16 @@ def Generate_HTML(title, codePR, triggerPR, submitURL, customizeURL):
   template = env.get_template('script/onboardTemplate')
   output = template.render(title=title, codePR=codePR, triggerPR=triggerPR, submitURL=submitURL, customizeURL=customizeURL);
   print(output)
+  return output
 
 
 if __name__ == "__main__":
-  print(f"Arguments count: {len(sys.argv)}")
+#   print(f"Arguments count: {len(sys.argv)}")
   if sys.argv[1] == "--help":
     print("usage: python script/emailsender.py [title] [codePR] [triggerPR] [submitURL] [customizeURL]")
     
-  for i, arg in enumerate(sys.argv):
-    print(f"Argument {i:>6}: {arg}")
+#   for i, arg in enumerate(sys.argv):
+#     print(f"Argument {i:>6}: {arg}")
   title = sys.argv[1]
   codePR = sys.argv[2]
   triggerPR = sys.argv[3]
