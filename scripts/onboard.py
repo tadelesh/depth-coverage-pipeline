0d0a000b6b91b8accdbabf98ee03ac99a469afa9
FAREAST\chunyu <chunyu@microsoft.com>
2021-02-03 13:04:15 +0800

    generate cli code

--- scripts/onboard.py
+++ scripts/onboard.py
@@ -12,11 +12,11 @@ import sys
 # output = template.render(users)
 # print(output)
 
-def Generate_HTML(title, codePR, triggerPR, submitURL, customizeURL):
+def Generate_HTML(title, head, codePR, triggerPR, submitURL, customizeURL):
   file_loader = FileSystemLoader('./')
   env = Environment(loader=file_loader)
   template = env.get_template('scripts/onboardTemplate')
-  output = template.render(title=title, codePR=codePR, triggerPR=triggerPR, submitURL=submitURL, customizeURL=customizeURL);
+  output = template.render(title=title, head=head, codePR=codePR, triggerPR=triggerPR, submitURL=submitURL, customizeURL=customizeURL);
   print(output)
   return output
 
@@ -24,15 +24,16 @@ def Generate_HTML(title, codePR, triggerPR, submitURL, customizeURL):
 if __name__ == "__main__":
 #   print(f"Arguments count: {len(sys.argv)}")
   if sys.argv[1] == "--help":
-    print("usage: python scripts/emailsender.py [title] [codePR] [triggerPR] [submitURL] [customizeURL]")
+    print("usage: python scripts/emailsender.py [title] [head] [codePR] [triggerPR] [submitURL] [customizeURL]")
     
 #   for i, arg in enumerate(sys.argv):
 #     print(f"Argument {i:>6}: {arg}")
   title = sys.argv[1]
-  codePR = sys.argv[2]
-  triggerPR = sys.argv[3]
-  submitURL = sys.argv[4]
+  head = sys.argv[2]
+  codePR = sys.argv[3]
+  triggerPR = sys.argv[4]
+  submitURL = sys.argv[5]
 
-  customizeURL = sys.argv[5]
+  customizeURL = sys.argv[6]
 
-  Generate_HTML(title, codePR, triggerPR, submitURL, customizeURL)
+  Generate_HTML(title, head, codePR, triggerPR, submitURL, customizeURL)
