6d1ae0c6c6c46270bcf7ae18c631f17e7ab9a873
FAREAST\chunyu <chunyu@microsoft.com>
2021-03-12 10:45:57 +0800

    redirect compile err to file

--- scripts/bash/compile.sh
+++ scripts/bash/compile.sh
@@ -18,9 +18,9 @@ fi
 
 echo -e "\e[32m[$(date -u)] LOG: [$RP]: make build"
 make build >> $GOPATH/../build.log
-echo $GOPATH
-cat $GOPATH/../build.log
 if [ "$?" != "0" ]; then
     echo -e "\e[31m[$(date -u)] ERROR: [$RP]: make build failed"
+    echo $GOPATH
+    cat $GOPATH/../build.log
     exit 1
 fi
